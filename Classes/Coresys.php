d6bd20765d1fa74462cb7a8f425c28af6137cd36
Sorethea OK <sorethea@gmail.com>
2023-02-20 11:46:55 +0700

    lam-dev

--- Classes/Coresys.php
+++ Classes/Coresys.php
@@ -2,9 +2,10 @@
 
 namespace Modules\Coresys\Classes;
 
+use Illuminate\Foundation\AliasLoader;
 use Modules\Coresys\Installers\FileInstaller;
 use Nwidart\Modules\FileRepository;
-use Nwidart\Modules\Laravel\Module;
+use Nwidart\Modules\Module;
 
 class Coresys extends Module
 {
@@ -16,11 +17,30 @@ class Coresys extends Module
 
     public function __construct(Container $app, string $name, $path)
     {
-        parent::__construct($app, $name, $path);
         $this->installer = $this->app[FileInstaller::class];
+        parent::__construct($app, $name, $path);
     }
 
     public static function install():string{
         return "This module is installed";
+    }
+
+    public function registerAliases(): void
+    {
+        $loader = AliasLoader::getInstance();
+        foreach ($this->get('aliases', []) as $aliasName => $aliasClass) {
+            $loader->alias($aliasName, $aliasClass);
+            dd($aliasClass);
+        }
+    }
+
+    public function registerProviders(): void
+    {
+
+    }
+
+    public function getCachedServicesPath(): string
+    {
+
     }
 }
