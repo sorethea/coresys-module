e9d90b7bbce9538b4303ef5a2aed7628521e6275
Sorethea OK <sorethea@gmail.com>
2023-02-20 14:06:48 +0700

    lam-dev

--- Classes/Coresys.php
+++ Classes/Coresys.php
@@ -14,30 +14,34 @@ class Coresys
         return \Module::find($name);
     }
 
-    public static function install($name): void {
-        $module = \Module::find($name);
+    public static function install($name): int {
+
         try{
+            $module = \Module::find($name);
             \DB::beginTransaction();
             \Artisan::call("module:migrate-refresh ".$name);
             \Artisan::call("module:seed ".$name);
             $module->enable();
             \DB::commit();
+            return $module->isEnable();
         }catch (\Throwable $e){
             \DB::rollBack();
-            report($e);
+            return $e->getCode();
         }
     }
 
-    public static function uninstall($name): void {
-        $module = \Module::find($name);
+    public static function uninstall($name): int
+    {
         try{
+            $module = \Module::find($name);
             \DB::beginTransaction();
             \Artisan::call("module:migrate-rollback ".$name);
             $module->disable();
             \DB::commit();
+            return $module->isEnable();
         }catch (\Throwable $e){
             \DB::rollBack();
-            report($e);
+            return $e->getCode();
         }
     }
 
